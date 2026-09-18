2a9ec5416e6d559ad608f5e231c83d5bf6b41ef6
ShrutiShete <shrutidev248@gmail.com>
2026-09-19 00:43:01 +0530

    safe

--- Backend/src/config/Database.js
+++
@@ -1,7 +0,0 @@
-const mongoose= require("mongoose");
-async function connectToDB(){
-    
-    await mongoose.connect(process.env.MONGO_DB)
-    console.log("Connected to MongoDB");
-}
-module.exports=connectToDB
